e5266338418f364618b8da7cc021ea2e476fd1f8
gonghf95 <1342735006@qq.com>
2020-11-23 21:07:18 +0800

    Callbacks.h

--- src/net/Callbacks.h
+++ src/net/Callbacks.h
@@ -15,6 +15,11 @@ typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
 typedef std::function<void ()> TimerCallback;
 typedef std::function<void (const TcpConnectionPtr&)> ConnectionCallback;
 
+typedef std::function<void (const TcpConnectionPtr&, Buffer*, Timestamp)> MessageCallback;
+
+void defaultConnectionCallback(const TcpConnectionPtr& conn);
+void defaultMessageCallback(const TcpConnectionPtr& conn, Buffer* buffer, Timestamp receiveTime);
+
 } // namespace net
 
 #endif /* CALLBACKS_H */
