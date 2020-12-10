15da774cf44a0da7e2a0d324b7b2435a12a60f06
gonghf95 <1342735006@qq.com>
2020-12-10 21:25:55 +0800

    stackTrace

--- src/base/CurrentThread.cc
+++ src/base/CurrentThread.cc
@@ -1,5 +1,9 @@
 #include "src/base/CurrentThread.h"
 
+#include <string>
+
+using std::string;
+
 namespace base
 {
 
@@ -14,7 +18,53 @@ static_assert(std::is_same<int, pid_t>::value, "pid_t should be int");
 
 string statckTrace(bool demangle)
 {
+	string stack;
+	const int max_frames = 200;
+	void* frame[max_frames];
+	int nptrs = ::backtrace(frame, max_frames);
+	char** strings = ::backtrace_symbols(frame, nptrs);
+	if(strings)
+	{
+		size_t len = 256;
+		char* demangled = demangle ? static_cast<char*>(::malloc(len)) : nullptr;
+		for(int i=1; i<nptrs; ++i)
+		{
+			if(demangle)
+			{
+				char* left_par = nullptr;
+				char* plus = nullptr;
+				for(char* p = strings[i]; *p; ++p)
+				{
+					if(*p == '(')
+						left_par = p;
+					else if(*p == '+')
+						plus = p;
+				}
 
+				if(left_par && plus)
+				{
+					*plus = '\0';
+					int status = 0;
+					char* ret = abi::__cxa_demangle(left_par+1, demangled, &len, &status);
+					*plus = '+';
+					if(status == 0)
+					{
+						demangled = ret;
+						stack.append(strings[i], left_par+1);
+						stack.append(demangled);
+						stack.append(plus);
+						stack.push_back('\n');
+						continue;
+					}
+				}
+			}
+			
+			stack.append(strings[i]);
+			stack.push_back('\n');
+		}
+		free(demangled);
+		free(strings);
+	}
 }
 
 } // namespace CurrentThread
