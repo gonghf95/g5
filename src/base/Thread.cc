f88e45de614bc9957a726c4719382e97fc8867ac
gonghf95 <1342735006@qq.com>
2020-12-16 21:23:53 +0800

    Thread.cc

--- src/base/Thread.cc
+++ src/base/Thread.cc
@@ -38,9 +38,63 @@ struct ThreadData
 	ThreadFunc func_;
 	string name_;
 	pid_t* tid_;
+	CountDownLatch* latch_;
 
+	ThreadData(ThreadFunc func, const string& name, pid* tid, CountDownLatch* latch) 
+		: func_(std::move(func)),
+		name_(name),
+		tid_(tid),
+		latch_(latch)
+	{
+	}
+
+	void runInThread()
+	{
+		*tid_ = base::CurrentThread::tid();
+		tid_ = NULL;
+		latch_->countDown();
+		latch_ = NULL;
+
+		base::CurrentThread::t_threadName = name_.empty() ? "g5c_thread" : name_.c_str();
+		::prctl(PR_SET_NAME, base::CurrentThread::t_threadName);
+		try
+		{
+			func_();
+			base::CurrentThread::t_threadName = "finished";
+		}
+		catch(const Exception& ex)
+		{
+			base::CurrentThread::t_threadName = "crashed";
+			fprintf(stderr, "exception caught in Thread %s\n", name_.c_str());
+			fprintf(stderr, "reason: %s\n", ex.what());
+			fprintf(stderr, "stack trace: %s\n", ex.stackTrace());
+			abort();
+		}
+		catch(const std::exception& ex)
+		{
+			base::CurrentThread::t_threadName = "crashed";
+			fprintf(stderr, "exception caught in Thread %s\n", name_.c_str());
+			abort();
+		}
+		catch(...)
+		{
+			base::CurrentThread::t_threadName = "crashed";
+			fprintf(stderr, "unknow exception caught in Thread %s\n", name_.c_str());
+			throw;
+		}
+	}
 };
 
+void* startThread(void* obj)
+{
+	ThreadData* data = static_cast<ThreadData*>(obj);
+	data->runInThread();
+	delete data;
+	return NULL;
 }
-}
+
+} // namespace detail
+
+
+} // namespace base
 
