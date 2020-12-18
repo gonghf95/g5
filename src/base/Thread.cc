9646a9dfc3bec08b05821c764d8460aa1474dc33
gonghf95 <1342735006@qq.com>
2020-12-18 20:01:00 +0800

    Thread.cc

--- src/base/Thread.cc
+++ src/base/Thread.cc
@@ -95,6 +95,86 @@ void* startThread(void* obj)
 
 } // namespace detail
 
+void CurrentThread::cacheTid()
+{
+	if(t_cachedTid == 0)
+	{
+		t_cachedTid = detail::gettid();
+		t_tidStringLength = snprintf(t_tidString, sizeof(t_tidString), "%5d ", t_cachedTid);
+	}
+}
+
+bool CurrentThread::isMainThread()
+{
+	return tid() == ::getpid();
+}
+
+void CurrentThread::sleepUsec(int64_t usec)
+{
+	struct timespec ts = {0, 0};
+	ts.tv_sec = static_cast<time_t>(usec / Timestamp::kMicroSecondsPerSecond);
+	ts.tv_nsec = static_cast<long>(usec % Timestamp::kMicroSecondsPerSecond * 1000);
+	::nanosleep(&ts, NULL);
+}
+
+AtomicInt32 Thread::numCreated_;
+
+Thread::Thread(ThreadFunc func, const string& n)
+	: started_(false),
+	joined_(false),
+	pthreadId_(0),
+	tid_(0),
+	func_(std::move(func)),
+	name_(n),
+	latch_(1)
+{
+	setDefaultName();
+}
+
+Thread::~Thread()
+{
+	if(started_ && !joined_)
+	{
+		pthread_detach(pthreadId_);
+	}
+}
+
+void Thread::setDefaultName()
+{
+	int num = numCreated_.incrementAndGet();
+	if(name_.empty())
+	{
+		char buf[32];
+		snprintf(buf, sizeof(buf), "Thread%d", num);
+		name_ = buf;
+	}
+}
+
+void Thread::start()
+{
+	assert(!started_);
+	started_ = true;
+
+	detail::ThreadData* data = new detail::ThreadData(func_, name_, &tid_, &latch_);
+	if(pthread_create(&pthreadId_, NULL, &detail::startThread, data))
+	{
+		started_ = false;
+		delete data;
+	}
+	else
+	{
+		latch_.wait();
+		assert(tid_ > 0);
+	}
+}
+
+void Thread::join()
+{
+	assert(started_);
+	assert(!joined_);
+	joined_ = true;
+	return pthread_join(pthreadId_, NULL);
+}
 
 } // namespace base
 
