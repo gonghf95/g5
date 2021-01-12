27d27936162c2ac18b0d7abb07959fda9c44da3b
gonghf95 <1342735006@qq.com>
2021-01-12 21:33:03 +0800

    Buffer

--- src/net/Buffer.h
+++ src/net/Buffer.h
@@ -1,18 +1,97 @@
 #ifndef BUFFER_H
 #define BUFFER_H
 
+#include "src/base/copyable.h"
+
 #include <string>
+#include <vector>
 
 namespace net
 {
 
-class Buffer
+class Buffer : public base::copyable
 {
 public:
-	Buffer();
-	virtual ~Buffer();
+	static const size_t kCheapPrepend = 8;
+	static const size_t kInitialSize = 1024;
 
-	const char* peek();
+	explicit Buffer(size_t initialSize = kInitialSize)
+		: buffer_(kCheapPrepend + initialSize),
+		readerIndex_(kCheapPrepend),
+		writerIndex_(kCheapPrepend)
+	{
+		assert(readableBytes() == 0);
+		assert(writableBytes() == initialSize);
+		assert(prependableBytes() == kCheapPrepend);
+	}
+
+	void swap(Buffer& rhs)
+	{
+		buffer_.swap(rhs.buffer_);
+		std::swap(readerIndex_, rhs.readerIndex_);
+		std::swap(writerIndex_, rhs.writerIndex_);
+	}
+
+	size_t readableBytes() const
+	{ return writerIndex_ - readerIndex_; }
+
+	size_t writableBytes() const
+	{ return buffer_.size() - writerIndex_; }
+
+	size_t prependableBytes() const
+	{ return readerIndex_; }
+
+	const char* peek()
+	{ return begin() + readerIndex_; }
+
+	const char* findCRLF() const
+	{
+		const char* crlf = std::search(peek(), beginWrite(), kCRLF, kCRLf+2);
+		return crlf == beginWrite() ? NULL : crlf;
+	}
+
+	const char* findCRLF(const char* start) const
+	{
+		assert(peek() <= start);
+		assert(start <= beginWrite());
+		const char* crlf = std::search(start, beginWrite(), kCRLF, kCRLF+2);
+		return crlf == beginWrite() ? NULL : crlf;
+	}
+
+	const char* findEOL() const
+	{
+		const void* eol = memchr(peek(), '\n', readableBytes());
+		return static_cast<const char*>(eol);
+	}
+
+	const char* findEOL(const char* start) const
+	{
+		assert(peek() <= start);
+		assert(start <= beginWrite());
+		const void* eol = memchr(start, '\n', beginWrite() - start);
+		return static_cast<const char*>(eol);
+	}
+
+	void retrieve(size_t len)
+	{
+		assert(len <= readableBytes());
+		if(len < readableBytes())
+		{
+			readerIndex_ += len;
+		}
+		else
+		{
+			retrieveAll();
+		}
+	}
+
+	void retrieveUntil(const char* end)
+	{
+		assert(peek() <= end);
+		assert(end <= beginWrite());
+		retrieve(end - peek());
+	}
+
 	int readableBytes() const;
 	void retrieve(int len);
 	void append(const std::string& data);
@@ -20,7 +99,11 @@ public:
 	std::string retrieveAllAsString();
 
 private:
-	std::string buf_;
+	std::vector<char> buffer_;
+	size_t readerIndex_;
+	size_t writerIndex_;
+
+	static const char kCRLF[];
 };
 
 } // namespace net
