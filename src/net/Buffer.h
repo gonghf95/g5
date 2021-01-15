969f054936397a76eec4dc26190cc1834fd95fde
gonghf95 <1342735006@qq.com>
2021-01-15 19:56:31 +0800

    Buffer

--- src/net/Buffer.h
+++ src/net/Buffer.h
@@ -92,13 +92,158 @@ public:
 		retrieve(end - peek());
 	}
 
-	int readableBytes() const;
-	void retrieve(int len);
-	void append(const std::string& data);
-	std::string retrieveAsString(size_t len);
-	std::string retrieveAllAsString();
+	void retrieveInt64()
+	{
+		retrieve(sizeof(int64_t));
+	}
+
+	void retrieveInt32()
+	{
+		retrieve(sizeof(int32_t));
+	}
+
+	void retrieveInt16()
+	{
+		retrieve(sizeof(int16_t));
+	}
+
+	void retrieveInt8()
+	{
+		retrieve(sizeof(int8_t));
+	}
+
+	void retrieveAll()
+	{
+		readerIndex_ = kCheapPrepend;
+		writerIndex_ = kCheapPrepend;
+	}
+
+	std::string retrieveAllAsString()
+	{
+		return retrieveAsString(readableBytes());
+	}
+
+	std::string retrieveAsString(size_t len)
+	{
+		assert(len <= readableBytes());
+		std::string result(peek(), len);
+		retrieve(len);
+		return result;
+	}
+
+	void append(const char* data, size_t len)
+	{
+		ensureWritableBytes(len);
+		std::copy(data, data+len, beginWrite());
+		writerIndex_ += len;
+	}
+	
+	void append(const void* data, size_t len)
+	{
+		append(static_cast<const char*>(data), len);
+	}
+
+	void ensureWritableBytes(size_t len)
+	{
+		if(writableBytes() < len)
+		{
+			makeSpace(len);
+		}
+		assert(len <= writableBytes());
+	}
+
+	char* beginWrite()
+	{
+		return begin() + writerIndex_;
+	}
+
+	const char* beginWrite() const
+	{
+		return begin() + writerIndex_;
+	}
+
+	void hasWritten(size_t len)
+	{
+		assert(len <= writableBytes());
+		writerIndex_ += len;
+	}
+
+	void unwrite(size_t len)
+	{
+		assert(len <= readableBytes());
+		readerIndex_ -= len;
+	}
+
+	int64_t readInt64() const
+	{
+		int64_t x = peekInt64();
+		retrieveInt64();
+		return x;
+	}
+
+	int32_t readInt32() const
+	{
+		int32_t x = peekInt32();
+		retrieveInt32();
+		return x;
+	}
+
+	int16_t readInt16() const
+	{
+		int16_t x = peekInt16();
+		retrieveInt16();
+		return x;
+	}
+
+	int8_t readInt8() const
+	{
+		int8_t x = peekInt8();
+		retrieveInt8();
+		return x;
+	}
+
+	int64_t peekInt64() const
+	{
+		assert(readableBytes() >= sizeof(int64_t));
+		int64_t x = 0;
+		::memcpy(&x, peek(), sizeof(int64_t));
+		return x;
+	}
+
+	int32_t peekInt32() const
+	{
+		assert(readableBytes() >= sizeof(int32_t));
+		int32_t x = 0;
+		::memcpy(&x, peek(), sizeof(int32_t));
+		return x;
+	}
+
+	int16_t peekInt16() const
+	{
+		assert(readableBytes() >= sizeof(int16_t));
+		int16_t x = 0;
+		::memcpy(&x, peek(), sizeof(int16_t));
+		return x;
+	}
+
+	int8_t peekInt8() const
+	{
+		assert(readableBytes() >= sizeof(int8_t));
+		int8_t x = *peek();
+		return x;
+	}
 
 private:
+	char* begin()
+	{ return &*buffer_.begin(); }
+	
+	const char* begin() const
+	{ return &*buffer_.begin(); }
+
+	void makeSpace(size_t len)
+	{
+	}
+
 	std::vector<char> buffer_;
 	size_t readerIndex_;
 	size_t writerIndex_;
