4009206d8028f2615f3877b15d80d9e1f2c80a3b
gonghf95 <1342735006@qq.com>
2021-01-26 21:15:44 +0800

    FileUtil

---
+++ src/net/FileUtil.h
@@ -0,0 +1,76 @@
+#ifndef BASE_FILEUTIL_H
+#define BASE_FILEUTIL_H
+
+#include "src/base/noncopyable.h"
+
+#include <string>
+
+namespace base
+{
+
+namespace FileUtil
+{
+
+class ReadSmallFile : noncopyable
+{
+public:
+	ReadSmallFile(std::string filename);
+	~ReadSmallFile();
+
+	template<typename String>
+	int readToString(int maxSize,
+					String* content,
+					int64_t* fileSize,
+					int64_t* modifyTime,
+					int64_t* createTime);
+	
+	int readToBuffer(int* size);
+
+	const char* buffer() const { return buf_; }
+
+	static const int kBufferSize = 64*1024;
+
+private:
+	int fd_;
+	int err_;
+	char buf[kBufferSize];
+};
+
+template<typename String>
+int readFile(std::string filename,
+			int maxSize,
+			String* content,
+			int64_t* fileSize = NULL,
+			int64_t* modifyTime = NULL,
+			int64_t* createTime = NULL)
+{
+	ReadSmallFile file(filename);
+	return file.readToString(maxSize, content, fileSize, modifyTime, createTime);
+}
+
+class AppendFile : noncopyable
+{
+public:
+	explicit AppendFile(std::string filename);
+	~AppendFile();
+
+	void append(const char* logline, size_t len);
+
+	void flush();
+
+	off_t writtenBytes() const { return writtenBytes_; }
+
+private:
+
+	size_t write(const char* logline, size_t len);
+
+	FILE* fp_;
+	char buffer_[64*1024];
+	off_t writtenBytes_;
+};
+
+} // namespace FileUtil
+
+} // namespace base
+
+#endif
