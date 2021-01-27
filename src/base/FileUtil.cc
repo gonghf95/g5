55fd9b344172f4209ffa9d12408f6b9bc8668c39
gonghf95 <1342735006@qq.com>
2021-01-27 21:01:42 +0800

    FileUtil

---
+++ src/base/FileUtil.cc
@@ -0,0 +1,90 @@
+#include "src/base/FileUtil.h"
+
+using namespace base;
+
+FileUtil::ReadSmallFile::ReadSmallFile(std::string filename)
+	: fd_(::open(filename.c_str(), O_RDONLY|O_CLOSEXEC)),
+	err_(0)
+{
+	buf_[0] = '\0';
+	if(fd_ < 0)
+	{
+		err_ = errno;
+	}
+}
+
+FileUtil::ReadSmallFile::~ReadSmallFile()
+{
+	if(fd_ >= 0)
+	{
+		::close(fd_);
+	}
+}
+
+template<typename String>
+int FileUtil::ReadSmallFile::readToString(int maxSize,
+										String* content,
+										int64_t* fileSize,
+										int64_t* modifyTime,
+										int64_t* createTime)
+{
+	static_assert(sizeof(off_t) == 8, "_FILE_OFFSET_BITS = 64");
+	assert(content != NULL);
+	int err = err_;
+	if(fd_ >= 0)
+	{
+		content->clear();
+
+		if(fileSize)
+		{
+			struct stat statbuf;
+			if(::fstat(fd_, &statbuf) == )
+			{
+				if(S_ISREG(statbuf.st_mode))
+				{
+					*fileSize = statbuf.st_size;
+					content->reserve(static_cast<int>(std::min(implicit_cast<int64_t>(maxSize),  *fileSize)));
+				}
+				else if(S_ISDIR(statbuf.st_mode))
+				{
+					err = EISDIR;
+				}
+
+				if(modifyTime)
+				{
+					*modifyTime = statbuf.st_mtime;
+				}
+
+				if(createTime)
+				{
+					*createTime = statbuf.st_ctime;
+				}
+			}
+			else
+			{
+				err = errno;
+			}
+		}
+
+		while(content->size() < implicit_cast<size_t>(maxSize))
+		{
+			size_t toRead = std::min(implicit_cast<size_t>(maxSize) - content->size(), sizeof(buf_));
+			ssize_t n = ::read(fd_, buf_, toRead);
+			if(n > 0)
+			{
+				content->append(buf_, n);
+			}
+			else
+			{
+				if(n<0)
+				{
+					err = errno;
+				}
+
+				break;
+			}
+		}
+	}
+
+	return err;
+}
