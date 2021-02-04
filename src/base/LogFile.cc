4ba6c60ee8353d81f998b635c2fcc367590f06f0
gonghf95 <1342735006@qq.com>
2021-02-04 21:11:16 +0800

    LogFile

---
+++ src/base/LogFile.cc
@@ -0,0 +1,41 @@
+#include "src/base/LogFile.h"
+
+#include <string>
+
+using namespace base;
+
+LogFile::LogFile(const std::string& basename, int flushEveryN)
+	: basename_(basename),
+	flushEveryN_(flushEveryN),
+	count_(0),
+	mutex_(new MutexLock)
+{
+	file_.reset(new AppendFile(basename));
+}
+
+LogFile::~LogFile()
+{
+}
+
+void LogFile::append(const char* logline, int len)
+{
+	MutexLockGuard lock(*mutex_);
+	append_unlocked(logline, len);
+}
+
+void LogFile::flush()
+{
+	MutexLockGuard lock(*mutex_);
+	file_->flush();
+}
+
+void LogFile::append_unlocked(const char* logline, int len)
+{
+	file_->append(logline, len);
+	++count_;
+	if(count >= flushEveryN_)
+	{
+		count_ = 0;
+		file_->flush();
+	}
+}
