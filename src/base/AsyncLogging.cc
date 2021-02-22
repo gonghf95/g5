88fe431da4667e2c9f1042860a1cac7866d38646
hongfu <1342735006@qq.com>
2021-02-22 23:33:27 +0800

    AsyncLogging

--- src/base/AsyncLogging.cc
+++ src/base/AsyncLogging.cc
@@ -4,11 +4,45 @@ AsyncLogging::AsyncLogging(const std::string basename, int flushInterval)
 	: flushInterval_(flushInterval),
 	running_(false),
 	basename_(basename),
-	thread_(std::bind(&AsyncLogging::threadFunc, this), "Logging")
+	thread_(std::bind(&AsyncLogging::threadFunc, this), "Logging"),
+	mutex_(),
+	cond_(mutex_),
+	currentBuffer_(new Buffer),
+	nextBuffer_(new Buffer),
+	buffers_(),
+	latch_(1)
 {
+	assert(basename_.size() > 1);
+	currentBuffer_->bzero();
+	nextBuffer_->bzero();
+	buffers_.reserve(16);
 }
 
 void AsyncLogging::append(const char* logline, int len)
 {
-	
+	MutexLockGuard lock(mutex_);
+	if(currentBuffer_->avail() > len)
+	{
+		currentBuffer_->append(logline, len);
+	}
+	else
+	{
+		buffers_.push_back(currentBuffer_);
+		currentBuffer_.reset();
+		if(nextBuffer_)
+		{
+			currentBuffer_ = std::move(nextBuffer_);
+		}
+		else
+		{
+			currentBuffer_.reset(new Buffer);
+		}
+		currentBuffer_->append(logline, len);
+		cond_.notify();
+	}
+}
+
+void AsyncLogging::threadFunc()
+{
+
 }
