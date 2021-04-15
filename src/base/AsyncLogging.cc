5ba4ac887ef09541d36f7726013d285d7b8cca2c
RD962 <RD962@bacongeer.com>
2021-04-15 19:16:19 +0800

    AsyncLogging.cc

--- src/base/AsyncLogging.cc
+++ src/base/AsyncLogging.cc
@@ -47,16 +47,67 @@ void AsyncLogging::append(const char* logline, int len)
 
 void AsyncLogging::threadFunc()
 {
-	assert(running_ == true);
-	LogFile output(basename_);
-	BufferPtr newBuffer1(new Buffer);
-	BufferPtr newBuffer2(new Buffer);
-	newBuffer1->bzero();
-	newBuffer2->bzero();
-	BufferVector buffersToWrite;
-	buffersToWrite.reserve(16);
-	while(running_)
-	{
-		
-	}
+    assert(running_ == true);
+    LogFile output(basename_);
+    BufferPtr newBuffer1(new Buffer);
+    BufferPtr newBuffer2(new Buffer);
+    newBuffer1->bzero();
+    newBuffer2->bzero();
+    BufferVector buffersToWrite;
+    buffersToWrite.reserve(16);
+    while(running_)
+    {
+        assert(newBuffer1 && newBuffer1->length() == 0);
+        assert(newBuffer2 && newBuffer2->length() == 0);
+        assert(buffersToWrite.empty());
+
+        {
+            MutexLockGuard lock(mutex_);
+            if (buffers_.empty())  // unusual usage!
+            {
+                cond_.waitForSeconds(flushInterval_);
+            }
+            buffers_.push_back(currentBuffer_);
+            currentBuffer_.reset();
+
+            currentBuffer_ = std::move(newBuffer1);
+            buffersToWrite.swap(buffers_);
+            if (!nextBuffer_) {
+                nextBuffer_ = std::move(newBuffer2);
+            }
+        }
+
+        assert(!buffersToWrite.empty());
+
+        if (buffersToWrite.size() > 25) {
+            buffersToWrite.erase(buffersToWrite.begin() + 2, buffersToWrite.end());
+        }
+
+        for (size_t i = 0; i < buffersToWrite.size(); ++i) {
+            // FIXME: use unbuffered stdio FILE ? or use ::writev ?
+            output.append(buffersToWrite[i]->data(), buffersToWrite[i]->length());
+        }
+        if (buffersToWrite.size() > 2) {
+            // drop non-bzero-ed buffers, avoid trashing
+            buffersToWrite.resize(2);
+        }
+
+        if (!newBuffer1) {
+            assert(!buffersToWrite.empty());
+            newBuffer1 = buffersToWrite.back();
+            buffersToWrite.pop_back();
+            newBuffer1->reset();
+        }
+
+        if (!newBuffer2) {
+            assert(!buffersToWrite.empty());
+            newBuffer2 = buffersToWrite.back();
+            buffersToWrite.pop_back();
+            newBuffer2->reset();
+        }
+
+        buffersToWrite.clear();
+        output.flush();
+    }
+    output.flush();
 }
